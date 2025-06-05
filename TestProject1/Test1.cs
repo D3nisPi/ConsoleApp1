5a4798980858ca422d66eb37c50ca28949997295
Сергей Воробьёв <sergey.vorobev_2004@mail.ru>
2025-06-06 00:46:15 +0300

    test2

--- TestProject1/Test1.cs
+++ TestProject1/Test1.cs
@@ -38,5 +38,12 @@ namespace TestProject1
 
             Assert.AreEqual(expectedResult, actualResult);
         }
+        public void TestMethod5()
+        {
+            int actualResult = _calculator.Add(6, 7);
+            int expectedResult = 7;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
     }
 }
