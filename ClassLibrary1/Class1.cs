cfea665ba9fcd95acbcaff713cd626c680fb41d4
D3nisPi <d3nis.p1shchik@gmail.com>
2025-06-06 00:16:04 +0300

    tests

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ClassLibrary1
+{
+    public class Calculator
+    {
+        public int Add(int a, int b) => a + b;
+    }
+}
---
+++ TestProject1/Test1.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary1;
+
+namespace TestProject1
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        private Calculator _calculator = new Calculator();
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int actualResult = _calculator.Add(5, 3);
+            int expectedResult = 8;
+ 
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
